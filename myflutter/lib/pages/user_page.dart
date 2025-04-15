58b270f148250fd8d937f08da365b2d35acb0511
kovalsofiia <126290290+kovalsofiia@users.noreply.github.com>
2025-04-15 20:22:19 +0300

    edited home page: toggle to see all/popular, added main_screen and navbar

---
+++ myflutter/lib/pages/user_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('User')),
+      body: Center(child: Text('User Page Content')),
+    );
+  }
+}
